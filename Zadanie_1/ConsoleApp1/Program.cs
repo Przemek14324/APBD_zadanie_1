fe4a5bb0672489d2875fa25fd5063c7196bd87f0
s24639 <s24639@pjwstk.edu.pl>
2024-03-23 23:39:58 +0100

    Modify loop variable name in CalculateAverage on feature-new

--- Zadanie_1/ConsoleApp1/Program.cs
+++ Zadanie_1/ConsoleApp1/Program.cs
@@ -8,11 +8,11 @@ public class Program
         Console.WriteLine("hello world");
         Console.WriteLine("Goodbye world :(");
         int[] sampleNumbers = { 1, 2, 3, 4, 5 };
-        double average = CalculateAverage(sampleNumbers);
+        double average = CalculateAverageZmiana(sampleNumbers);
         Console.WriteLine($"The average is: {average}");
         Console.WriteLine("koniec zadan");
     }
-    public static double CalculateAverage(int[] numbers)
+    public static double CalculateAverageZmiana(int[] numbers)
     {
         return numbers.Average();
     }
@@ -20,6 +20,6 @@ public class Program
     {
         return numbers.Max();
     }
-    // To jest nowy komentarz
+  
 
 }
