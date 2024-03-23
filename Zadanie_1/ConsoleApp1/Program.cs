024f2a314f3c9194822384cb9dbcd347887da4dd
s24639 <s24639@pjwstk.edu.pl>
2024-03-23 23:35:00 +0100

    modyfikacja koncowa

--- Zadanie_1/ConsoleApp1/Program.cs
+++ Zadanie_1/ConsoleApp1/Program.cs
@@ -7,5 +7,19 @@ public class Program
     {
         Console.WriteLine("hello world");
         Console.WriteLine("Goodbye world :(");
+        int[] sampleNumbers = { 1, 2, 3, 4, 5 };
+        double average = CalculateAverage(sampleNumbers);
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine("koniec zadan");
     }
+    public static double CalculateAverage(int[] numbers)
+    {
+        return numbers.Average();
+    }
+    public static int CalculateMax(int[] numbers)
+    {
+        return numbers.Max();
+    }
+    // To jest nowy komentarz
+
 }
